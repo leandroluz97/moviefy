c44e3417366d89d273f0c83c32110600ae2409e0
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-10 13:37:13 +0000

    added register dependency injection

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,5 +1,11 @@
-import { RegisterView } from "./register.view";
+import { HttpClient } from '@/infrastructure/http/httpClient';
+import { RegisterView } from './register.view';
+import { RegisterService } from '@/services/Authentication/AuthenticationService';
+import { useRegisterModel } from './register.model';
 
 export const Register = () => {
-  return <RegisterView/>
+    const httClient = new HttpClient();
+    const registerService = new RegisterService(httClient);
+    const register = useRegisterModel({ registerService });
+    return <RegisterView {...register} />;
 };
