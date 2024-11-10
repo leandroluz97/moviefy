8175fc4808f5ab2aa38fade4f4ec60d1e69a05c6
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-10 13:36:33 +0000

    update routes with all available routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
-import { protectedRoutes } from "./protected";
-import { publicRoutes } from "./public";
-import { CommonRoutes } from "./common";
-
+import { createBrowserRouter } from 'react-router-dom';
+import { protectedRoutes } from './protected';
+import { publicRoutes } from './public';
+import { CommonRoutes } from './common';
 
 const appRoutes = () => {
-  const auth = {user:true}
-  const route = auth.user ? protectedRoutes : publicRoutes;
-  return createBrowserRouter([...route, ...CommonRoutes])
+    // const auth = { user: false };
+    // const route = auth.user ? protectedRoutes : publicRoutes;
+    return createBrowserRouter([...publicRoutes, ...protectedRoutes, ...CommonRoutes]);
 };
 
-export const routes = appRoutes()
+export const routes = appRoutes();
