3285269848a8e34a0f82cdd16bf02d7b2ab7c654
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-10 12:48:46 +0000

    added QueryClient provider

--- client/src/provider/index.tsx
+++ client/src/provider/index.tsx
@@ -1,10 +1,19 @@
+import React from 'react'
+import { queryClient } from "@/lib/react-query";
 import { routes } from "@/routes";
-import React from "react";
+import { QueryClientProvider } from "react-query";
 import {  RouterProvider } from "react-router-dom";
+import { Loading } from '@/components/Loading';
 
 // type AppProviderProps =  {
 // children: React.ReactNode
 // }
 export const AppProvider = () => {
-  return  <RouterProvider router={routes} />
+    return (
+            <React.Suspense fallback={<Loading />}>
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={routes} />
+                </QueryClientProvider>
+            </React.Suspense>
+        )
 };
