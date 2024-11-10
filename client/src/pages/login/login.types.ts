80e07e94da7e4ba8e03707419d0dc4d22a4fa661
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-10 12:48:07 +0000

    added login types

---
+++ client/src/pages/login/login.types.ts
@@ -0,0 +1,11 @@
+import { User } from '@/domain/User';
+import { ILoginOptions } from '@/services/Authentication/AuthenticationService';
+import { AxiosError } from 'axios';
+import { UseMutationOptions } from 'react-query';
+
+export type LoginForm = {
+    email: string;
+    password: string;
+};
+
+export type MutationConfig = UseMutationOptions<User, AxiosError, ILoginOptions>;
